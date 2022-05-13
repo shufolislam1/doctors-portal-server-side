a85efd7d2c5e5b187ab261075bbbdf81be290dcf
shufol islam <shufolislam1255@gmail.com>
2022-05-14 03:51:08 +0600

    basic server half done

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
+
+
+const uri = "mongodb+srv://doctor_web:<password>@cluster0.oho4v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+app.get('/', (req, res) => {
+  res.send('Hello doctors portal!')
+})
+
+app.listen(port, () => {
+  console.log(`doctors portal listening on port ${port}`)
+})
