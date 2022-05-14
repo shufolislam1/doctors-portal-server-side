edfc24efc4237fcef3ceb1d0158e81db7140cb78
shufol islam <shufolislam1255@gmail.com>
2022-05-14 20:16:48 +0600

    basic server all done

--- index.js
+++ index.js
@@ -9,13 +9,30 @@ app.use(cors())
 app.use(express.json())
 
 
-const uri = "mongodb+srv://doctor_web:<password>@cluster0.oho4v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oho4v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+
+async function run() {
+    try{
+      await client.connect();
+
+      const servicesCollection = client.db('doctors_portal').collection('services')
+
+      app.get('/services', async(req, res) => {
+        const query = {}
+        const cursor = servicesCollection.find(query)
+        const services = await cursor.toArray()
+        res.send(services)
+      })
+    }
+    
+    finally{
+
+    }
+}
+
+run().catch(console.dir)
 
 
 app.get('/', (req, res) => {
